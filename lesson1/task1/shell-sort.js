20ae91fd98f157554736b467965c3e337d04cb05
baltser-v <vadim.baltser99@gmail.com>
2026-04-02 22:06:31 +0700

    shell-sort

---
+++ lesson1/task1/shell-sort.js
@@ -0,0 +1,16 @@
+const shellSort = (arr) => {
+    let gap = Math.floor(arr.length / 2);
+    while (gap > 0) {
+        for (let i = gap; i < arr.length; i++) {
+            let key = arr[i];
+            let j = i;
+            while (j >= gap && arr[j - gap] > key) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = key;
+        }
+        gap = Math.floor(gap / 2);
+    }
+    return arr;
+};
